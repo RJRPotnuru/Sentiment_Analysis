c6cafe03e62e35db1710379379ce372a4bc06c55
RJRPotnuru <86295035+RJRPotnuru@users.noreply.github.com>
2021-07-14 11:39:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 st.title('Sentiment Analysis')
 import joblib
 clf=joblib.load('Sentiment Analysis')
@@ -6,3 +7,9 @@ ip=st.text_input('Enter your text')
 op=clf.predict([ip])
 if st.button('Predict'):
   st.title(op[0])
+  if op[0]=='positive':
+    image=Image.open('happy.jpeg')
+    st.image(image,width=250)
+  if op[0]=='negative': 
+    image=Image.open('sad.jpeg')
+    st.image(image,width=250)
